9a55d600dd1a5a626d86f32a74005ba0458ba80e
Scholarelli <212111464+Scholarelli@users.noreply.github.com>
2025-09-04 10:57:47 +0200

    flushinput if resize

--- controls.cpp
+++ controls.cpp
@@ -4,13 +4,15 @@
 
 controls get_user_input() {
     int c = getch();
-    flushinp();
     switch (c) {
         case KEY_UP:    case 'w': return controls::MOVE_UP;
         case KEY_DOWN:  case 's': return controls::MOVE_DOWN;
         case KEY_LEFT:  case 'a': return controls::MOVE_LEFT;
         case KEY_RIGHT: case 'd': return controls::MOVE_RIGHT;
-        case KEY_RESIZE: return controls::RESIZE;
+        case KEY_RESIZE: {
+            flushinp();
+            return controls::RESIZE;
+        }
         case ' ': case 'p': case 10:  return controls::PAUSE;
         case 'f': return controls::FREEZE;
         case 'q':                 return controls::QUIT;
